ceb7b56ea4a888c57d4220f7143c9e30c167b4b2
root <root@DESKTOP-8FSC3E1.localdomain>
2019-11-07 17:43:53 +0100

    Game functions done

--- js/game.js
+++ js/game.js
@@ -10,7 +10,7 @@ const Game = {
         RIGHT_KEY: 39,
         LEFT_KEY: 37
     },
-    hasAcorn : false ,
+    hasAcorn: false,
 
 
 
@@ -23,9 +23,9 @@ const Game = {
         this.height = window.innerHeight;
         this.canvas.width = this.width;
         this.canvas.height = this.height;
-        
-      
-        
+
+
+
 
 
         this.start();
@@ -35,26 +35,38 @@ const Game = {
         this.reset()
         this.interval = setInterval(() => {
             this.clear();
-            
+            console.log(this.player.posY0);
+
             this.framesCounter++;
-            
+
             this.drawAll();
             this.moveAll();
             if (this.isCollision()) {
                 this.inPLatform()
-            } else {
+            } 
+            
+            else {
                 this.player.posY0 = 480
             }
-            if(this.framesCounter%300==0)this.generateEnemies();
-            if(this.framesCounter%5==0)this.generateCoin();
-            if(this.isCollisionCoin()) this.Winner
-            if(this.isCollisionButcher()) this.gameOver()
+            // if (this.isCollisionWinner()) {
+            //     this.inPLatformWinner()
+            // } else {
+            //     this.player.posY0 = 480
+            // }
+            if(this.isCollisionWinner() && this.hasAcorn){
+                alert("you have saved yourself from being eaten")
+            }
+           
+            if (this.framesCounter % 300 == 0) this.generateEnemies();
+            if (this.framesCounter % 5 == 0) this.generateCoin();
+            if (this.isCollisionCoin()) this.Winner
+            if (this.isCollisionButcher()) this.gameOver()
             // this.inPLatform();
-            if (this.isCollisionButcher()){
+            if (this.isCollisionButcher()) {
                 console.log("muerto")
             }
-            if(this.isCollisionCoin()){
-                this.hasAcorn = true 
+            if (this.isCollisionCoin()) {
+                this.hasAcorn = true
                 console.log(this.hasAcorn)
             }
 
@@ -63,37 +75,38 @@ const Game = {
     },
 
 
-    reset: function() {
+    reset: function () {
         this.background = new Background(this.ctx, this.width, this.height)
         this.player = new Player(this.ctx, 90, 70, "img/pig-right-move.png", "img/pig-left-move.png", this.width, this.height, this.playerKeys);
         this.coins = [];
-        this.obstacles = [];
+        // this.winner = [];
         this.enemies = [];
         this.winners = [];
+        this.obstacles = [];
         this.generateObstacles();
         this.generateCoin();
         this.generateWinner();
         console.log(this.coins)
 
-        
+
 
     },
 
 
 
-    clear: function() {
+    clear: function () {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
     },
 
 
-    drawAll: function() {
-      
+    drawAll: function () {
+
         this.background.draw();
         this.player.draw(this.framesCounter);
-       if(!this.hasAcorn){
-        this.coins.forEach(coin => coin.draw());
+        if (!this.hasAcorn) {
+            this.coins.forEach(coin => coin.draw());
 
-       }
+        }
 
         this.obstacles.forEach(obstacle => obstacle.draw());
         this.winners.forEach(winner => winner.draw());
@@ -105,33 +118,30 @@ const Game = {
 
     moveAll: function () {
         this.player.move();
-        this.enemies.forEach(enemy=>enemy.move())
+        this.enemies.forEach(enemy => enemy.move())
 
     },
 
-    generateCoin: function() {
-        if(this.coins.length < 2) {
-            this.coins.push(new Coin(this.ctx, "img/acorn-custom.png",50));
+    generateCoin: function () {
+        if (this.coins.length < 2) {
+            this.coins.push(new Coin(this.ctx, "img/acorn-custom.png", 50));
 
         }
-        
+
     },
 
-    generateEnemies: function() {
-        if(this.enemies.length<4){
+    generateEnemies: function () {
+        if (this.enemies.length < 4) {
 
-            this.enemies.push(new Enemy(this.ctx, 60, 100, "img/butcher-png.png", 
-            // "img/butcher-left.svg",
-             this.width, this.height, 60,100));
+            this.enemies.push(new Enemy(this.ctx, 60, 100, "img/butcher-png.png",
+                // "img/butcher-left.svg",
+                this.width, this.height, 60, 100));
         }
         // console.log(this.enemies);
 
     },
 
-    generateObstacles: function () {
-
-
-
+    generateObstacles: function (){
 
         if (this.obstacles.length <= 10) {
             for (i = 0; i <= 10; i++) {
@@ -153,37 +163,37 @@ const Game = {
                     this.obstacles.push(new Obstacle(this.ctx, 100, 25, this.width, this.height, this.width * 0.9, this.height * 0.7))
                 } else if (i === 8) {
                     this.obstacles.push(new Obstacle(this.ctx, 70, 25, this.width, this.height, this.width * 0.2, this.height * 0.6))
-                    
+
                 } else
                     this.obstacles.push(new Obstacle(this.ctx, 100, 25, this.width, this.height, this.width * 0.5, this.height * 0.6))
 
             }
         }
+    
 
     },
 
     generateWinner: function () {
 
         if (this.winners.length <= 1) {
-        
-         this.winners.push(new Winner(this.ctx, 70, 25, this.width, this.height, this.width * 0.2, this.height * 0.6));
-              
-               
+         this.winners.push(new Winner(this.ctx, 70, 25, this.width, this.height, this.width * 0.9, this.height * 0.6));
+
+
         }
 
     },
 
-    
+
     gameOver: function () {
         clearInterval(this.interval)
     },
 
-    Winner: function(){
-        if(this.hasAcorn = true && this.player){
-            clearInterval(this.interval)
-        }
-        
-    },
+    // Winner: function () {
+    //     if (this.hasAcorn = true && this.player) {
+    //         clearInterval(this.interval)
+    //     }
+
+    // },
 
     isCollision: function () {
         // colisiones genéricas
@@ -192,43 +202,46 @@ const Game = {
         return this.obstacles.some(obstacle => (this.player.posX + this.player.width > obstacle.posX && obstacle.posX + obstacle.width > this.player.posX && this.player.posY + this.player.height > obstacle.posY && obstacle.posY + obstacle.height > this.player.posY))
     },
 
-    isCollisionButcher: function (){
+    isCollisionButcher: function () {
         return this.enemies.some(enemy => (this.player.posX + this.player.width > enemy.posX && enemy.posX + enemy.width > this.player.posX && this.player.posY + this.player.height > enemy.posY && enemy.posY + enemy.height > this.player.posY))
 
     },
 
-    isCollisionCoin: function(){
+    isCollisionCoin: function () {
         // return this.coins.some(coin => (this.player.posX + this.player.width > coin.posX && coin.posX + coin.width > this.player.posX && this.player.posY + this.player.height > coin.posY && coin.posY + coin.height > this.player.posY))
-        return this.coins.some(coin=>this.player.posX + this.player.width > coin.posX && this.player.posY < coin.posY && this.player.posX < coin.posX + 50 )
+        return this.coins.some(coin => this.player.posX + this.player.width > coin.posX && this.player.posY < coin.posY && this.player.posX < coin.posX + 50)
     },
 
-    isCollisionWinner : function(){
-        
+    isCollisionWinner: function () {
+        return this.winners.some(winner => (this.player.posX + this.player.width > winner.posX && winner.posX + winner.width > this.player.posX && this.player.posY + this.player.height > winner.posY && winner.posY + winner.height > this.player.posY))
     },
 
-    
 
- 
+
+
 
     inPLatform: function () {
         this.obstacles.forEach((obstacle) => {
-            if (this.player.posX + 40  > obstacle.posX && this.player.posX - this.player.width/2  < obstacle.posX + obstacle.width  && this.player.posY < obstacle.posY - 65) {
+            if (this.player.posX + 40 > obstacle.posX && this.player.posX - this.player.width / 2 < obstacle.posX + obstacle.width && this.player.posY < obstacle.posY - 65) {
 
                 this.player.posY0 = obstacle.posY - 65;
             }
-
-
         })
-        //     if(this.player.posX + this.player.width > this.obstacles[2].posX && this.player.posX < this.obstacles[2].posX + this.obstacles[2].width && this.player.posY < this.obstacles[2].posY){
 
-        //         this.player.posY0 = 240
-        //        console.log(this.player.posY0)
-        //     } else {
-        //         this.player.posY0 = 400
-        //     }
-        //    }
+    },
+
+
+    inPLatformWinner: function () {
+       
+        this.winners.forEach((winner) => {
+            if (this.player.posX + 40 > winner.posX && this.player.posX - this.player.width / 2 < winner.posX + winner.width && this.player.posY < winner.posY - 65) {
+                this.player.posY0 = winner.posY - 65;
+        
+            }
+        })
     }
-}
+    
+};
 
 
 

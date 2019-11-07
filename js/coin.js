8e3ec65fdaee5d980b62f4657349927bc04b65a4
root <root@DESKTOP-8FSC3E1.localdomain>
2019-11-07 13:20:11 +0100

    Added acorns

---
+++ js/coin.js
@@ -0,0 +1,21 @@
+class Coin {
+    constructor(ctx,image, height) {
+        this.ctx = ctx;
+
+        this.image = new Image();
+        this.image.src = image;
+        this.height = height;
+        this.posX = 400;
+        this.posY = 170;
+
+    }
+
+    draw() {
+        
+        this.ctx.drawImage(this.image, this.posX, this.posY, 50, 50);
+        
+    }
+
+
+
+}
--- js/game.js
+++ js/game.js
@@ -14,6 +14,7 @@ const Game = {
 
 
 
+
     init: function () {
         this.canvas = document.getElementById("canvas");
         this.ctx = this.canvas.getContext("2d");
@@ -32,7 +33,7 @@ const Game = {
         this.interval = setInterval(() => {
             this.clear();
             this.framesCounter++;
-
+            
             this.drawAll();
             this.moveAll();
             if (this.isCollision()) {
@@ -41,11 +42,17 @@ const Game = {
                 this.player.posY0 = 480
             }
             if(this.framesCounter%300==0)this.generateEnemies();
+            if(this.framesCounter%5==0)this.generateCoin();
             
             // this.inPLatform();
             if (this.isCollisionButcher()){
                 console.log("muerto")
             }
+            if(this.isCollisionCoin()){
+                console.log("money cabron")
+                this.coins = []
+            }
+
 
         }, 1000 / this.fps)
     },
@@ -54,9 +61,12 @@ const Game = {
     reset: function () {
         this.background = new Background(this.ctx, this.width, this.height)
         this.player = new Player(this.ctx, 90, 70, "img/pig-right-move.png", "img/pig-left-move.png", this.width, this.height, this.playerKeys);
+        this.coins = [];
         this.obstacles = [];
         this.enemies = [];
         this.generateObstacles();
+        this.generateCoin();
+        console.log(this.coins)
 
         
 
@@ -72,8 +82,8 @@ const Game = {
     drawAll: function() {
         this.background.draw();
         this.player.draw(this.framesCounter);
-        // this.enemy.draw(this.framesCounter);
         this.obstacles.forEach(obstacle => obstacle.draw());
+        this.coins.forEach(coin => coin.draw());
         this.enemies.forEach(enemy => enemy.draw(this.framesCounter));
 
     },
@@ -83,6 +93,14 @@ const Game = {
         this.player.move();
         this.enemies.forEach(enemy=>enemy.move())
 
+    },
+
+    generateCoin: function() {
+        if(this.coins.length < 2) {
+            this.coins.push(new Coin(this.ctx, "img/acorn-custom.png",50));
+
+        }
+        
     },
 
     generateEnemies: function() {
@@ -101,20 +119,29 @@ const Game = {
         // me genera 8 elementos en el array Whaaaaat??
 
 
-        if (this.obstacles.length <= 5) {
-            for (i = 0; i <= 5; i++) {
+        if (this.obstacles.length <= 10) {
+            for (i = 0; i <= 10; i++) {
                 if (i === 0) {
-                    this.obstacles.push(new Obstacle(this.ctx, 100, 25, this.width, this.height, this.width * 0.8, this.height * 0.5))
+                    this.obstacles.push(new Obstacle(this.ctx, 100, 25, this.width, this.height, this.width * 0.8, this.height * 0.6))
                 } else if (i === 1) {
-                    this.obstacles.push(new Obstacle(this.ctx, 100, 25, this.width, this.height, this.width * 0.5, this.height * 0.5))
+                    this.obstacles.push(new Obstacle(this.ctx, 100, 25, this.width, this.height, this.width * 0.5, this.height * 0.6))
                 } else if (i === 2) {
                     this.obstacles.push(new Obstacle(this.ctx, 80, 25, this.width, this.height, this.width * 0.35, this.height * 0.6))
                 } else if (i === 3) {
-                    // this.obstacles.push(new Obstacle(this.ctx, 100, 25, this.width, this.height, this.width * 0.2, this.height * 0.5))
+                    this.obstacles.push(new Obstacle(this.ctx, 100, 25, this.width, this.height, this.width * 0.2, this.height * 0.6))
                 } else if (i === 4) {
-                    this.obstacles.push(new Obstacle(this.ctx, 10, 25, this.width, this.height, this.width * 0.1, this.height * 0.5))
+                    this.obstacles.push(new Obstacle(this.ctx, 100, 25, this.width, this.height, this.width * 0.1, this.height * 0.5))
+                } else if (i === 5) {
+                    this.obstacles.push(new Obstacle(this.ctx, 100, 55, this.width, this.height, this.width * 0.2, this.height * 0.6))
+                } else if (i === 6) {
+                    this.obstacles.push(new Obstacle(this.ctx, 100, 55, this.width, this.height, this.width * 0.4, this.height * 0.6))
+                } else if (i === 7) {
+                    this.obstacles.push(new Obstacle(this.ctx, 100, 25, this.width, this.height, this.width * 0.9, this.height * 0.7))
+                } else if (i === 8) {
+                    this.obstacles.push(new Obstacle(this.ctx, 70, 25, this.width, this.height, this.width * 0.2, this.height * 0.6))
+                    
                 } else
-                    this.obstacles.push(new Obstacle(this.ctx, 100, 25, this.width, this.height, this.width * 0.5, this.height * 0.5))
+                    this.obstacles.push(new Obstacle(this.ctx, 100, 25, this.width, this.height, this.width * 0.5, this.height * 0.6))
 
             }
         }
@@ -127,6 +154,7 @@ const Game = {
     isCollision: function () {
         // colisiones genéricas
         // (p.x + p.w > o.x && o.x + o.w > p.x && p.y + p.h > o.y && o.y + o.h > p.y )
+
         return this.obstacles.some(obstacle => (this.player.posX + this.player.width > obstacle.posX && obstacle.posX + obstacle.width > this.player.posX && this.player.posY + this.player.height > obstacle.posY && obstacle.posY + obstacle.height > this.player.posY))
     },
 
@@ -135,7 +163,10 @@ const Game = {
 
     },
 
-
+    isCollisionCoin: function(){
+        // return this.coins.some(coin => (this.player.posX + this.player.width > coin.posX && coin.posX + coin.width > this.player.posX && this.player.posY + this.player.height > coin.posY && coin.posY + coin.height > this.player.posY))
+        return this.coins.some(coin=>this.player.posX + this.player.width > coin.posX && this.player.posY < coin.posY && this.player.posX < coin.posX + 50 )
+    },
 
 
 
